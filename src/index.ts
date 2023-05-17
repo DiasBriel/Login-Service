a701767d8df6d049d5d9811bcbbef069fdd5ed9e
Gabriel Dias <g.dias1@hotmail.com>
2023-05-17 19:04:56 -0300

    Handling errors

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,17 @@
+import 'express-async-errors'
 import "reflect-metadata";
 import "./shared/containers";
 import express, { Express } from "express";
 import { AppDataSource } from "./data-source";
 import routes from "./routes/router";
+import { errorMiddleware } from "./middlewares/error";
 
 AppDataSource.initialize().then(() => {
   const app: Express = express();
+
   app.use(express.json());
   app.use(routes);
+  app.use(errorMiddleware);
 
   return app.listen(process.env.PORT);
 })
